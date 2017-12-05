42f136db563c44240342eaa68a036f6147b62844
Eduardo Delira <edelira1@gmail.com>
2017-12-05 03:29:10 +0000

    Edit down Errors and Exceptions to one For loop

--- main.py
+++ main.py
@@ -12,50 +12,26 @@ while True:
 
 	nextStaNm = xmldoc.getElementsByTagName('nextStaNm')
 
-	try:
-		train_zero = nextStaNm[0].firstChild.nodeValue
-		print train_zero
-	except IndexError:
-		train_zero = 'null'
-		print train_zero
-	try:
-		train_one = nextStaNm[1].firstChild.nodeValue
-		print train_one
-	except IndexError:
-		train_one = 'null'
-	try:
-		train_two = nextStaNm[2].firstChild.nodeValue
-		print train_two
-	except IndexError:
-		train_two = 'null'
-	try:
-		train_three = nextStaNm[3].firstChild.nodeValue
-		print train_three
-	except IndexError:
-		train_three = 'null'
-	try:
-		train_four = nextStaNm[4].firstChild.nodeValue
-		print train_four
-	except IndexError:
-		train_four = 'null'
-	try:
-		train_five = nextStaNm[5].firstChild.nodeValue
-		print train_five
-	except IndexError:
-		train_five = 'null'
-	try:
-		train_six = nextStaNm[6].firstChild.nodeValue
-		print train_six
-	except IndexError:
-		train_six = 'null'
+	train_zero = train_one = train_two = train_three = train_four = train_five = train_six = ""
+
+	train_check = [train_zero,train_one,train_two,train_three,train_four,train_five,train_six]
+
+	j = 0
+	for train in train_check:
+		try:
+			train = nextStaNm[j].firstChild.nodeValue
+			j += 1
+			print train
+			print j
+		except IndexError:
+			i = 'null'
+			print i
 	# prints all base:OBS_VALUE in the XML doc
 
 	print "Active trains below"
 	trains = []
 	trains.extend((train_zero,train_one,train_two,train_three,train_four,train_five,train_six))
 	print  ', '.join(trains)
-
-
 
 	trainList = ['54th/Cermak','Kostner','Pulaski','Central Park','Western',
 	             'Damen','18th','Polk','Ashland','Morgan','Clinton','Clark/Lake',
@@ -65,4 +41,4 @@ while True:
 	for i in trainList:
 	    if i in trains:
 	        print i
-
+	time.sleep(5)
